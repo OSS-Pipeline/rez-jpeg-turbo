b3fee5399116aed03466df84e1ffd8bfb8a73cf3
Joshua Senouf <joshua.senouf@gmail.com>
2020-12-26 17:08:58 +1300

    Fixed libs paths.

--- package.py
+++ package.py
@@ -42,10 +42,10 @@ uuid = "jpeg_turbo-{version}".format(version=str(version))
 
 def commands():
     env.PATH.prepend("{root}/bin")
-    env.LD_LIBRARY_PATH.prepend("{root}/lib64")
-    env.PKG_CONFIG_PATH.prepend("{root}/lib64/pkgconfig")
+    env.LD_LIBRARY_PATH.prepend("{root}/lib")
+    env.PKG_CONFIG_PATH.prepend("{root}/lib/pkgconfig")
 
     # Helper environment variables.
     env.JPEG_TURBO_BINARY_PATH.set("{root}/bin")
     env.JPEG_TURBO_INCLUDE_PATH.set("{root}/include")
-    env.JPEG_TURBO_LIBRARY_PATH.set("{root}/lib64")
+    env.JPEG_TURBO_LIBRARY_PATH.set("{root}/lib")
